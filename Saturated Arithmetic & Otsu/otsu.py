cbbe88a700a434ca8070bd514874c0cf774b0f6f
MRTCc <51177049+MRTCc@users.noreply.github.com>
2023-04-01 21:13:25 +0200

    Provato, così funziona

--- Saturated Arithmetic & Otsu/otsu.py
+++ Saturated Arithmetic & Otsu/otsu.py
@@ -9,39 +9,22 @@ im = data.camera()
 im = resize(im, (im.shape[0] // 2, im.shape[1] // 2), mode='reflect', preserve_range=True, anti_aliasing=True).astype(np.uint8)
 im = torch.from_numpy(im)
 
-# variance_b = Wb*Wf (ub-uf)^2
-# iterate thresholds and compute variance_b[t]
-# the correct threshold is the one corresponding to higher variance_b
+hist = torch.zeros([256])
+var_b = torch.zeros([256])
 
-hist = torch.zeros([255])
-var_b = torch.zeros([255])
-
-for i in range(255):
+for i in range(256):
     hist[i] = torch.sum(im == i)
     
-#plt.bar(np.arange(0,255, 1, dtype=int), hist)
-#plt.show()
+for i in range(256):
+    wb = torch.sum(hist[:i+1])
+    wf = torch.sum(hist[i+1:])
     
-for i in range(254):
-    wb = torch.sum(hist[:i+1]) / torch.numel(hist)
-    wf = torch.sum(hist[i+1:]) / torch.numel(hist)
+    if wb == 0 or wf == 0:
+        var_b[i] = -1
+    else:
+        ub = torch.sum(hist[:i+1] * torch.arange(start=0, end=i+1)) / wb
+        uf = torch.sum(hist[i+1:] * torch.arange(start=i+1, end=256)) / wf
+        var_b[i] = wb * wf * torch.pow(ub - uf, 2)
     
-    ub = torch.sum(hist[:i+1] * i)/ torch.numel(hist[:i+1])
-    uf = torch.sum(hist[i+1:] * i)/ torch.numel(hist[i+1:])
-    
-    var_b[i] = wb*wf*((ub -uf)**2)
-    
-thresh = int(torch.argmax(var_b)) 
-
-out = torch.zeros_like(im)
-
-out[im > thresh] = 255
-out[im <= thresh] = 0
-
-print(thresh)
-
-plt.imshow(out, cmap='gray')
-plt.show()
-
-pass
+out = int(torch.argmax(var_b))
 
